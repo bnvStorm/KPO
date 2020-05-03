dc1335ec8e17a915da941dbcc31bf3af7818ee31
HerMan <infabls.com@gmail.com>
2020-05-03 16:25:56 +0600

    change relative links to absolute for speed up purposes

--- includes/footer.php
+++ includes/footer.php
@@ -16,23 +16,23 @@
   <!-- /footer --> 
 </div>
 <!-- .body-wrapper --> 
-<script src="../style/js/jquery.min.js"></script> 
-<script src="../style/js/bootstrap.min.js"></script> 
-<script src="../style/js/twitter-bootstrap-hover-dropdown.min.js"></script> 
-<script src="../style/js/jquery.themepunch.plugins.min.js"></script> 
-<script src="../style/js/jquery.themepunch.revolution.min.js"></script> 
-<script src="../style/js/jquery.easytabs.min.js"></script> 
-<script src="../style/js/owl.carousel.min.js"></script> 
-<script src="../style/js/jquery.isotope.min.js"></script> 
-<script src="../style/js/jquery.fitvids.js"></script> 
-<script src="../style/js/jquery.fancybox.pack.js"></script> 
-<script src="../style/js/fancybox/helpers/jquery.fancybox-thumbs.js?v=1.0.2"></script> 
-<script src="../style/js/fancybox/helpers/jquery.fancybox-media.js?v=1.0.0"></script> 
-<script src="../style/js/jquery.slickforms.js"></script> 
-<script src="../style/js/instafeed.min.js"></script> 
-<script src="../style/js/retina.js"></script> 
-<script src="../style/js/google-code-prettify/prettify.js"></script> 
-<script src="../style/js/scripts.js"></script>
+<script src="https://style/js/jquery.min.js"></script> 
+<script src="https://style/js/bootstrap.min.js"></script> 
+<script src="https://style/js/twitter-bootstrap-hover-dropdown.min.js"></script> 
+<script src="https://style/js/jquery.themepunch.plugins.min.js"></script> 
+<script src="https://style/js/jquery.themepunch.revolution.min.js"></script> 
+<script src="https://style/js/jquery.easytabs.min.js"></script> 
+<script src="https://style/js/owl.carousel.min.js"></script> 
+<script src="https://style/js/jquery.isotope.min.js"></script> 
+<script src="https://style/js/jquery.fitvids.js"></script> 
+<script src="https://style/js/jquery.fancybox.pack.js"></script> 
+<script src="https://style/js/fancybox/helpers/jquery.fancybox-thumbs.js?v=1.0.2"></script> 
+<script src="https://style/js/fancybox/helpers/jquery.fancybox-media.js?v=1.0.0"></script> 
+<script src="https://style/js/jquery.slickforms.js"></script> 
+<script src="https://style/js/instafeed.min.js"></script> 
+<script src="https://style/js/retina.js"></script> 
+<script src="https://style/js/google-code-prettify/prettify.js"></script> 
+<script src="https://style/js/scripts.js"></script>
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
