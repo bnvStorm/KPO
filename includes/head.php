cc950c3796aac484b7b462f7e5c78a8ea6e48604
HerMan <infabls.com@gmail.com>
2020-05-18 20:11:58 +0600

    add social metatags

--- includes/head.php
+++ includes/head.php
@@ -5,9 +5,27 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="shortcut icon" href="https://rpo.kz/style/images/favicon.png">
 
-<title>Промотход</title>
-<meta name="description" content="">
+<title>Промотход сервис - контейнеры для мусора для бизнеса</title>
+<meta name="description" content="Экологичные контейнерные площадки для вашего бизнеса. От проекта до внедрения">
 <meta name="robots" content="index,follow">
+ <!-- social -->
+<meta property="og:title" content="Промотход Сервис">
+<meta property="og:type" content="article">
+<meta property="og:url" content="https://rpo.kz/">
+<meta property="og:image" content="https://rpo.kz/style/images/social.jpg">
+<meta property="og:site_name" content="Промотход Сервис">
+<!-- <meta property="fb:admins" content="">  -->
+<meta property="og:description" content="Экологичные контейнерные площадки для вашего бизнеса">
+<meta property="og:locale" content="ru_RU">
+
+<meta name="twitter:card" content="summary_large_image">
+<!-- <meta name="twitter:site" content="@Freepik_Vectors">
+<meta name="twitter:creator" content="@Freepik_Vectors"> -->
+<meta name="twitter:title" content="Промотход Сервис">
+<meta name="twitter:image:src" content="https://rpo.kz/style/images/social.jpg">
+<meta name="twitter:url" content="https://rpo.kz/">
+<meta name="twitter:description" content="Экологичные контейнерные площадки для вашего бизнеса">
+  <!-- social -->
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
       <script src="style/js/html5shiv.js"></script>
